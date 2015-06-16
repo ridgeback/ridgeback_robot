bf85ccba1793ce880e15eb7e2dfcac620046f0d4
Tony Baltovski <tony.baltovski@gmail.com>
2015-06-16 10:08:15 -0400

    More matching joints and CAN IDs.

--- ridgeback_base/src/ridgeback_hardware.cpp
+++ ridgeback_base/src/ridgeback_hardware.cpp
@@ -53,9 +53,9 @@ RidgebackHardware::RidgebackHardware(ros::NodeHandle& nh, ros::NodeHandle& pnh,
   ros::V_string joint_names = boost::assign::list_of("front_left_wheel")
       ("front_right_wheel")("rear_left_wheel")("rear_right_wheel");
 
-   std::vector<uint8_t> joint_canids = boost::assign::list_of(5)(4)(3)(2);
+   std::vector<uint8_t> joint_canids = boost::assign::list_of(5)(4)(2)(3);
    // 5 4 3 2
-   std::vector<float> joint_directions = boost::assign::list_of(-1)(1)(1)(-1);
+   std::vector<float> joint_directions = boost::assign::list_of(-1)(1)(-1)(1);
 
 
   for (unsigned int i = 0; i < joint_names.size(); i++)
